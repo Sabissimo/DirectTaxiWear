97ddbdcba3c30df04970a167b810d0f66a5cdd7c
Sabissimo <sabanadze@gmail.com>
2015-04-13 22:05:35 +0400

    New connection

--- mobile/src/main/java/com/sabik/android/directtaxiwear/TaxiMain.java
+++ mobile/src/main/java/com/sabik/android/directtaxiwear/TaxiMain.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.StrictMode;
 import android.util.Log;
@@ -22,7 +21,12 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,11 +35,8 @@ import java.util.List;
  */
 public class TaxiMain{
 
-    private static final long CONNECTION_TIME_OUT_MS = 100;
-
     private GoogleApiClient client;
     private String nodeId;
-
     private static final String TAG = "TOASTME";
     private static Context context;
 
@@ -99,6 +100,8 @@ public class TaxiMain{
                     Log.d(TAG, "Doesn't work! 3");
                     replyToWatch("notok");
                 }
+
+                //testingt();
             }
         }).start();
     }
@@ -122,4 +125,31 @@ public class TaxiMain{
             Log.d(TAG, "Device offline");
         }
     }
+
+    public void testingt(){
+        try {
+            String urlParameters  = "param1=a&param2=b&param3=c";
+            byte[] postData       = urlParameters.getBytes( Charset.forName( "UTF-8" ));
+            int    postDataLength = postData.length;
+            String request        = "http://taxi.d1erp.com/addorder.php";
+            URL    url            = new URL( request );
+            HttpURLConnection cox= (HttpURLConnection) url.openConnection();
+            cox.setDoOutput( true );
+            cox.setDoInput ( true );
+            cox.setInstanceFollowRedirects( false );
+            cox.setRequestMethod( "POST" );
+            cox.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+            cox.setRequestProperty( "charset", "utf-8");
+            cox.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+            cox.setUseCaches( false );
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
 }
+
